8ec1a7409723c52cb0d2c603e28f20e0180af2e4
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-04 14:39:37 +0530

    backtick error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import path from "path";
 import { fileURLToPath } from 'url';
-import { dirname } from `path`;
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
